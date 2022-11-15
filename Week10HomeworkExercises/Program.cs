b70af4f8b3559dffdc6deda776bee7830753a96f
Kristina Palmer <thekristinadawn@gmail.com>
2022-11-15 16:17:27 -0600

    Started Homework

--- Week10HomeworkExercises/Program.cs
+++ Week10HomeworkExercises/Program.cs
@@ -10,6 +10,49 @@ namespace Week10HomeworkExercises
     {
         static void Main(string[] args)
         {
+            //#1 Console read input of numbers and break at empty. Print sum and average of sequence. 
+
+            //List<int> numbers = new List<int>();
+            //while (true) //while(input != string.Empty)
+            //{
+            //    int input = Convert.ToInt32(Console.ReadLine());
+            //    if (input < 0)
+            //    {
+            //        break;
+            //    }
+            //    if (input == int.Empty)
+            //    {
+            //        break;
+            //    }
+            //    numbers.Add(input);
+            //}
+            //fills a list called numbers until user enters " -1 "
+
+            List<int> numbers = new List<int>();
+            bool inputNum;
+            int number;
+            string input = Console.ReadLine();
+            inputNum = int.TryParse(input, out number);
+            while (input != string.Empty)
+
+            {
+                {
+
+                    if (number < 0)
+                    {
+                        break;
+                    }
+                    numbers.Add(number);
+
+                }
+            }
+
+            Console.WriteLine(number);
+            Console.WriteLine("numbers" + numbers);
+
+
+
+
         }
     }
 }
